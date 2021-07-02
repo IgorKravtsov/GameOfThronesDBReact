f29c504e8ae9107cdb37e2c9c18bb3f678946462
Igor <ihor.kravtsov@nure.ua>
2021-07-02 23:27:40 +0300

    Random character added

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,18 +1,53 @@
 import './App.css';
 import {BrowserRouter as Router} from "react-router-dom";
-import {Container} from "reactstrap";
+import {Col, Container, Row} from "reactstrap";
 import Header from "../header/header";
+import {Component} from "react";
+import gotService from "../../services/gotService";
+import RandomChar from "../randomChar/randomChar";
 
-function App() {
-  return (
-      <Router>
-          <div className='app'>
-              <Container>
-                  <Header />
-              </Container>
-          </div>
-      </Router>
-  );
+export default class App extends Component {
+    gotService = new gotService();
+
+    state = {
+        showRandomChar: true,
+        error: false,
+        selectedHouse: 20
+    };
+
+    toggleRandomChar = () => {
+        this.setState((state) => {
+            return {
+                showRandomChar: !state.showRandomChar
+            }
+        });
+    };
+
+    render() {
+        const char = this.state.showRandomChar ? <RandomChar/> : null;
+
+
+        return (
+            <Router>
+                <div className='app'>
+                    <Container>
+                        <Header />
+                    </Container>
+                    <Container>
+                        <Row>
+                            <Col lg={{size: 5, offset: 0}}>
+                                {char}
+                                <button
+                                    className="toggle-btn"
+                                    onClick={this.toggleRandomChar}>Toggle random character
+                                </button>
+                            </Col>
+                        </Row>
+                    </Container>
+                </div>
+            </Router>
+        )
+    }
+
+
 }
-
-export default App;
